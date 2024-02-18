814eaaffdeb5c5f8fc8fa316b41358480e10c11c
Raaaandy <103336195+Raaaandy@users.noreply.github.com>
2024-02-17 19:01:36 -0800

    hw3 A3 a

---
+++ hw3-A/homeworks/kernel_bootstrap/main.py
@@ -0,0 +1,240 @@
+from typing import Tuple, Union
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from utils import load_dataset, problem
+
+
+def f_true(x: np.ndarray) -> np.ndarray:
+    """True function, which was used to generate data.
+    Should be used for plotting.
+
+    Args:
+        x (np.ndarray): A (n,) array. Input.
+
+    Returns:
+        np.ndarray: A (n,) array.
+    """
+    return 6 * np.sin(np.pi * x) * np.cos(4 * np.pi * x ** 2)
+
+
+@problem.tag("hw3-A")
+def poly_kernel(x_i: np.ndarray, x_j: np.ndarray, d: int) -> np.ndarray:
+    """Polynomial kernel.
+
+    Given two indices a and b it should calculate:
+    K[a, b] = (x_i[a] * x_j[b] + 1)^d
+
+    Args:
+        x_i (np.ndarray): An (n,) array. Observations (Might be different from x_j).
+        x_j (np.ndarray): An (m,) array. Observations (Might be different from x_i).
+        d (int): Degree of polynomial.
+
+    Returns:
+        np.ndarray: A (n, m) matrix, where each element is as described above (see equation for K[a, b])
+
+    Note:
+        - It is crucial for this function to be vectorized, and not contain for-loops.
+            It will be called a lot, and it has to be fast for reasonable run-time.
+        - You might find .outer functions useful for this function.
+            They apply an operation similar to xx^T (if x is a vector), but not necessarily with multiplication.
+            To use it simply append .outer to function. For example: np.add.outer, np.divide.outer
+    """
+    K = (np.multiply.outer(x_i, x_j) + 1)**d
+    return K
+
+
+@problem.tag("hw3-A")
+def rbf_kernel(x_i: np.ndarray, x_j: np.ndarray, gamma: float) -> np.ndarray:
+    """Radial Basis Function (RBF) kernel.
+
+    Given two indices a and b it should calculate:
+    K[a, b] = exp(-gamma*(x_i[a] - x_j[b])^2)
+
+    Args:
+        x_i (np.ndarray): An (n,) array. Observations (Might be different from x_j).
+        x_j (np.ndarray): An (m,) array. Observations (Might be different from x_i).
+        gamma (float): Gamma parameter for RBF kernel. (Inverse of standard deviation)
+
+    Returns:
+        np.ndarray: A (n, m) matrix, where each element is as described above (see equation for K[a, b])
+
+    Note:
+        - It is crucial for this function to be vectorized, and not contain for-loops.
+            It will be called a lot, and it has to be fast for reasonable run-time.
+        - You might find .outer functions useful for this function.
+            They apply an operation similar to xx^T (if x is a vector), but not necessarily with multiplication.
+            To use it simply append .outer to function. For example: np.add.outer, np.divide.outer
+    """
+    K = -gamma * np.subtract.outer(x_i, x_j)**2
+    K = np.exp(K)
+    return K
+
+
+@problem.tag("hw3-A")
+def train(
+    x: np.ndarray,
+    y: np.ndarray,
+    kernel_function: Union[poly_kernel, rbf_kernel],  # type: ignore
+    kernel_param: Union[int, float],
+    _lambda: float,
+) -> np.ndarray:
+    """Trains and returns an alpha vector, that can be used to make predictions.
+
+    Args:
+        x (np.ndarray): Array of shape (n,). Observations.
+        y (np.ndarray): Array of shape (n,). Targets.
+        kernel_function (Union[poly_kernel, rbf_kernel]): Either poly_kernel or rbf_kernel functions.
+        kernel_param (Union[int, float]): Gamma (if kernel_function is rbf_kernel) or d (if kernel_function is poly_kernel).
+        _lambda (float): Regularization constant.
+
+    Returns:
+        np.ndarray: Array of shape (n,) containing alpha hat as described in the pdf.
+    """
+    K = kernel_function(x, x, kernel_param)
+    a_hat = np.linalg.solve(K + _lambda * np.eye(len(K)), y)
+    return a_hat
+
+
+@problem.tag("hw3-A", start_line=1)
+def cross_validation(
+    x: np.ndarray,
+    y: np.ndarray,
+    kernel_function: Union[poly_kernel, rbf_kernel],  # type: ignore
+    kernel_param: Union[int, float],
+    _lambda: float,
+    num_folds: int,
+) -> float:
+    """Performs cross validation.
+
+    In a for loop over folds:
+        1. Set current fold to be validation, and set all other folds as training set.
+        2, Train a function on training set, and then get mean squared error on current fold (validation set).
+    Return validation loss averaged over all folds.
+
+    Args:
+        x (np.ndarray): Array of shape (n,). Observations.
+        y (np.ndarray): Array of shape (n,). Targets.
+        kernel_function (Union[poly_kernel, rbf_kernel]): Either poly_kernel or rbf_kernel functions.
+        kernel_param (Union[int, float]): Gamma (if kernel_function is rbf_kernel) or d (if kernel_function is poly_kernel).
+        _lambda (float): Regularization constant.
+        num_folds (int): Number of folds. It should be either len(x) for LOO, or 10 for 10-fold CV.
+
+    Returns:
+        float: Average loss of trained function on validation sets across all folds.
+    """
+    fold_size = len(x) // num_folds
+    valid_loss = []
+    for i in range(num_folds):
+        start_idx = i * fold_size
+        end_idx = (i + 1) * fold_size
+        x_train = np.concatenate((x[:start_idx], x[end_idx:]))
+        x_valid = x[start_idx: end_idx]
+        y_train = np.concatenate((y[:start_idx], y[end_idx:]))
+        y_valid = y[start_idx: end_idx]
+        a_hat = train(x_train, y_train, kernel_function=kernel_function, kernel_param=kernel_param)
+        prediction = a_hat @ kernel_function(x, x, kernel_param)
+        valid_loss.append(np.subtract(y_valid, prediction)**2)
+    return np.mean(valid_loss)
+
+@problem.tag("hw3-A")
+def rbf_param_search(
+    x: np.ndarray, y: np.ndarray, num_folds: int
+) -> Tuple[float, float]:
+    """
+    Parameter search for RBF kernel.
+
+    There are two possible approaches:
+        - Grid Search - Fix possible values for lambda, loop over them and record value with the lowest loss.
+        - Random Search - Fix number of iterations, during each iteration sample lambda from some distribution and record value with the lowest loss.
+
+    Args:
+        x (np.ndarray): Array of shape (n,). Observations.
+        y (np.ndarray): Array of shape (n,). Targets.
+        num_folds (int): Number of folds. It should be len(x) for LOO.
+
+    Returns:
+        Tuple[float, float]: Tuple containing best performing lambda and gamma pair.
+
+    Note:
+        - You do not really need to search over gamma. 1 / (median(dist(x_i, x_j)^2) for all unique pairs x_i, x_j in x
+            should be sufficient for this problem. That being said you are more than welcome to do so.
+        - If using random search we recommend sampling lambda from distribution 10**i, where i~Unif(-5, -1)
+        - If using grid search we recommend choosing possible lambdas to 10**i, where i=linspace(-5, -1)
+    """
+    gamma = 1 / np.median(np.subtract.outer(x, x)**2)
+    _lambda_V = []
+    loss_V = []
+
+    for i in np.linspace(-5,-1, 10):
+        _lambda = 10**i
+        loss = cross_validation(x, y, rbf_kernel, gamma, _lambda, num_folds=num_folds)
+        _lambda_V.append(_lambda)
+        loss_V.append(loss)
+    min_idx = np.argmin(loss_V)
+    return _lambda_V[min_idx], gamma
+
+@problem.tag("hw3-A")
+def poly_param_search(
+    x: np.ndarray, y: np.ndarray, num_folds: int
+) -> Tuple[float, int]:
+    """
+    Parameter search for Poly kernel.
+
+    There are two possible approaches:
+        - Grid Search - Fix possible values for lambdas and ds.
+            Have nested loop over all possibilities and record value with the lowest loss.
+        - Random Search - Fix number of iterations, during each iteration sample lambda, d from some distributions and record value with the lowest loss.
+
+    Args:
+        x (np.ndarray): Array of shape (n,). Observations.
+        y (np.ndarray): Array of shape (n,). Targets.
+        num_folds (int): Number of folds. It should be either len(x) for LOO, or 10 for 10-fold CV.
+
+    Returns:
+        Tuple[float, int]: Tuple containing best performing lambda and d pair.
+
+    Note:
+        - If using random search we recommend sampling lambda from distribution 10**i, where i~Unif(-5, -1)
+            and d from distribution [5, 6, ..., 24, 25]
+        - If using grid search we recommend choosing possible lambdas to 10**i, where i=linspace(-5, -1)
+            and possible ds to [5, 6, ..., 24, 25]
+    """
+    loss_V = []
+    _lambda_V = []
+    d_V = []
+    for i in np.linalg(-5, -1, 10):
+        _lambda = 10**i
+        for d in range(5, 26):
+            d_V.append(d)
+            _lambda_V.append(_lambda)
+            loss = cross_validation(x, y, poly_kernel, d, _lambda, num_folds=num_folds)
+            loss_V.append(loss)
+    min_idx = np.argmin(loss_V)
+    return _lambda_V[min_idx], d_V[min_idx]
+
+
+@problem.tag("hw3-A", start_line=1)
+def main():
+    """
+    Main function of the problem
+
+    It should:
+        A. Using x_30, y_30, rbf_param_search and poly_param_search report optimal values for lambda (for rbf), gamma, lambda (for poly) and d.
+            Note that x_30, y_30 has been loaded in for you. You do not need to use (x_300, y_300) or (x_1000, y_1000).
+        B. For both rbf and poly kernels, train a function using x_30, y_30 and plot predictions on a fine grid
+
+    Note:
+        - In part b fine grid can be defined as np.linspace(0, 1, num=100)
+        - When plotting you might find that your predictions go into hundreds, causing majority of the plot to look like a flat line.
+            To avoid this call plt.ylim(-6, 6).
+    """
+    (x_30, y_30), (x_300, y_300), (x_1000, y_1000) = load_dataset("kernel_bootstrap")
+    _lambda_rbf, gamma = rbf_param_search(x_30, y_30, len(x_30))
+    _lambda_poly, d = poly_param_search(x_30, y_30, len(x_30))
+    print("lambda for rbf, gamma, lambda for poly, d", _lambda_rbf, gamma, _lambda_poly, d)
+
+
+if __name__ == "__main__":
+    main()
